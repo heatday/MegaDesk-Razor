c61d854636f4ac948a78dc648512a1ff68cea2b6
Dallin Olson <86103274+DallinO@users.noreply.github.com>
2023-06-07 10:36:14 -0600

    Context & Quote Class Update

---
+++ MegaDesk Razor/Data/MegaDeskContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using MegaDesk_Razor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MegaDesk_Razor.Data
+{
+    public class MegaDeskContext : DbContext
+    {
+        public MegaDeskContext(DbContextOptions<MegaDeskContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Quote> Quotes { get; set; }
+    }
+}
---
+++ MegaDesk Razor/Models/Quote.cs
@@ -0,0 +1,14 @@
+﻿namespace MegaDesk_Razor.Models
+{
+    public class Quote
+    {
+        public string customerName { get; set; }
+        public string deliveryType { get; set; }
+        public int width { get; set; }
+        public int depth { get; set; }
+        public int drawerCount { get; set; }
+        public string material { get; set; }
+        public int cost { get; set; }
+
+    }
+}
